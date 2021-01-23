6bfe8cad77877d33675cec6610d292044d82de05
ShantanuLamba <sl.group958@gmail.com>
2021-01-23 15:55:43 +0530

    initial commit

---
+++ yt.py
@@ -0,0 +1,23 @@
+#remove # from either one of the codes to run
+
+#download through youtube_dl library
+#import youtube_dl
+
+#ydl_opts = {}
+
+#def downloadvideo():
+#    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+#        ydl.download([link])
+#link = input("Paste video link here: ")
+#downloadvideo()
+
+
+#download through pytube library
+#from pytube import YouTube
+
+#def download():
+#    YouTube(url).streams.first().download()
+
+#url = input("Paste link: ")
+#download()
+
